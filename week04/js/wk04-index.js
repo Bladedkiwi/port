ba58cc3b3c9b154000cd01f722b53cbdce9d640a
Bladedkiwi <bladedkiwi@byui.edu>
2020-10-10 22:29:38 -0500

    examples + notes

--- week04/js/wk04-index.js
+++ week04/js/wk04-index.js
@@ -2,5 +2,11 @@ const links = [
 	{label: "Week 4 Notes",
 		url:"./notes.html"},
 		{label: "Tic Tac Toe",
-		url:"./team.html"}
+		url:"./team.html"},
+		{label: "CSS Variables + JS",
+		url:"./css-variables.html"},
+		{label: "Flex Gallery",
+		url:"./flex-panel-gallery.html"},
+		{label: "JS/CSS Clock",
+		url:"./js-css-clock.html"}
 ];
