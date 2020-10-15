22ea2f91b58c57d8cab59b06976874ec5d9a08c0
Bladedkiwi <bladedkiwi@byui.edu>
2020-10-15 13:14:36 -0500

    week 5 index

--- week05/js/wk05-index.js
+++ week05/js/wk05-index.js
@@ -1,4 +1,12 @@
 const links = [
 	{label: "Week 5 Notes",
-		url:"./notes.html"}
+		url:"./notes.html"},
+		{label: "ExamplePad",
+		url:"./examplePad.html"},
+		{label: "Team Activity",
+		url:"./team.html"},
+		{label: "Upgraded Note It!",
+		url:"./note-taker.html"},
+		{label: "To Do List: Challeng One",
+		url:"./todo/index.html"}
 ]
