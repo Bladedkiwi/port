efa7312edbbb08c5a8ecb8b08e8b6d54daa72906
Bladedkiwi <bladedkiwi@byui.edu>
2020-10-31 22:53:32 -0500

    updates notes, examplepad, JS30 examples

--- week07/js/wk07-index.js
+++ week07/js/wk07-index.js
@@ -1,1 +1,5 @@
-const links = [{ label: "Notes", url: "./notes.html" }];
+const links = [
+	{ label: "Notes", url: "./notes.html" },
+	{ label: "Array Cardio", url: "./arrayCardioDay2.html" },
+	{ label: "TypeAhead Ajax", url: "./typeAheadAjax.html" },
+];
