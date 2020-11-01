c51467ecf54992ffeab5c70f0ee8e5e3d754ab48
Bladedkiwi <bladedkiwi@byui.edu>
2020-11-01 08:02:09 -0600

    updated links

--- week07/js/wk07-index.js
+++ week07/js/wk07-index.js
@@ -1,5 +1,7 @@
 const links = [
 	{ label: "Notes", url: "./notes.html" },
+	{ label: "ExamplePad", url: "./examplePad.html" },
 	{ label: "Array Cardio", url: "./arrayCardioDay2.html" },
 	{ label: "TypeAhead Ajax", url: "./typeAheadAjax.html" },
+	{ label: "Team Activity", url: "./teamActivity/team.html" }
 ];
