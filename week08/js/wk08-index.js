76ce2b4b1edd792bd1f2fdcf4e5cf0039454f5d7
Bladedkiwi <bladedkiwi@byui.edu>
2020-11-02 18:44:43 -0600

    added a recursive function to accessing pages

--- week08/js/wk08-index.js
+++ week08/js/wk08-index.js
@@ -1,1 +1,4 @@
-const links = [{ label: "Notes", url: "./notes.html" }];
+const links = [
+	{ label: "Notes", url: "./notes.html" },
+	{ label: "Swapi-API Tests", url: "./swapi-APIAjaxExample.html" },
+];
